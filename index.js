55a8f27c4c176e77964921ffa92113044a7bbf5a
Sheena Jessee <calalilym@yahoo.com>
2019-03-08 21:55:50 -0700

    add

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+var path = require('path');
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.get('/math', (req, res) => {
+		var oper1 = Number(req.query.value1);
+		var oper2 = Number(req.query.value2);
+		var operation = req.query.operation;
+		var total = getTotal(oper1, oper2, operation);
+
+		console.log("Total = " + total);
+
+		var param = {
+			total: total
+		};
+
+		res.render("pages/math", param);
+	})
+	.get('/math_service', (req, res) => {
+		var oper1 = Number(req.query.value1);
+		var oper2 = Number(req.query.value2);
+		var operation = req.query.operation;
+		var total = getTotal(oper1, oper2, operation);
+
+		console.log("Total = " + total);
+
+		var param = {
+			total: total
+		};
+
+		res.write(JSON.stringify(param));
+		res.end();
+	})
+	.listen(PORT, () => console.log(`Listening on ${PORT}`))
