f934eb4b982326eaa92f058828b241885ad7fe40
Sheena Jessee <calalilym@yahoo.com>
2019-03-09 07:36:54 -0700

    add

--- index.js
+++ index.js
@@ -7,33 +7,113 @@ express()
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
-	.get('/math', (req, res) => {
-		var oper1 = Number(req.query.value1);
-		var oper2 = Number(req.query.value2);
-		var operation = req.query.operation;
-		var total = getTotal(oper1, oper2, operation);
+	.get('/getRate', (req, res) => {
+		var weight = Number(req.query.weight);
+		var postType = req.query.postType;
 
-		console.log("Total = " + total);
-
-		var param = {
-			total: total
-		};
-
-		res.render("pages/math", param);
-	})
-	.get('/math_service', (req, res) => {
-		var oper1 = Number(req.query.value1);
-		var oper2 = Number(req.query.value2);
-		var operation = req.query.operation;
-		var total = getTotal(oper1, oper2, operation);
-
-		console.log("Total = " + total);
+		var rate = calculateRate(weight, postType)
+		console.log(rate);
 
 		var param = {
-			total: total
+			postType: postType,
+			weight: weight,
+			rate: rate
+			
 		};
 
-		res.write(JSON.stringify(param));
-		res.end();
+		res.render("pages/getRate", param);
 	})
+	
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
+function calculateRate(weight, type) {
+	switch (type) {
+		case "stamped":
+		    console.log("it made it here")
+			if (weight < 1) {
+				console.log("this is the weight")
+				return 0.55;
+				
+			}
+			else if (weight > 1 && weight < 3) {
+				return 0.70;
+			}
+			else if (weight > 2 && weight < 4) {
+				return .85;
+			}
+			else if (weight > 3 && weight <= 3.5) {
+				return 1.00;
+			}
+			
+			break;
+		case "metered":
+			if (weight < 1) {
+				return 0.50;
+			}
+			else if (weight > 1 && weight < 3) {
+				return 0.65;
+			}
+			else if (weight > 2 && weight < 4) {
+				return .80;
+			}
+			else if (weight > 3 && weight <= 3.5) {
+				return .95;
+			}
+			break;
+		case "flats":
+			if (weight < 1) {
+				return 1.00;
+			}
+			else if (weight > 1 && weight < 3) {
+				return 1.15;
+			}
+			else if (weight > 2 && weight < 4) {
+				return 1.30;
+			}
+			else if (weight > 3 && weight < 5) {
+				return 1.45;
+			}
+			else if (weight > 4 && weight < 6) {
+				return 1.60;
+			}
+			else if (weight > 5 && weight < 7) {
+				return 1.75;
+			}
+			else if (weight > 6 && weight < 8) {
+				return 1.90;
+			}
+			else if (weight > 7 && weight < 9) {
+				return 2.05;
+			}
+			else if (weight > 8 && weight < 10) {
+				return 2.20;
+			}
+			else if (weight > 9 && weight < 11) {
+				return 2.35;
+			}
+			else if (weight > 10 && weight < 12) {
+				return 2.50;
+			}
+			else if (weight > 11 && weight < 13) {
+				return 2.65;
+			}
+			else if (weight > 12 && weight <= 13) {
+				return 2.80;
+			}
+			
+			break;
+		case "package":
+			if (weight < 5) {
+				return 3.66;
+			}
+			else if (weight > 4 && weight < 9) {
+				return 4.39;
+			}
+			else if (weight > 8 && weight < 13) {
+				return 5.19;
+			}
+			else if (weight == 13) {
+				return 5.71;
+			}
+			break;
+	}
+}
